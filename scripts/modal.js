c791e685d0bb85f96d70fe41749858fcad8d59d8
talgatmendekov <talgatmendekov@gmail.com>
2022-07-12 11:22:34 +0600

    create edit and delete modals

---
+++ scripts/modal.js
@@ -0,0 +1,31 @@
+const modalWindowWrapper = document.createElement('div')
+modalWindowWrapper.className = 'modalWindowWrapper'
+
+const modalWindowContainer = document.createElement('div')
+modalWindowContainer.className = 'modalWindowContainer'
+
+const deleteModalWindow = document.createElement('div')
+deleteModalWindow.className = 'deleteModalWindow'
+
+const editModalWindow = document.createElement('div')
+editModalWindow.className = 'editModalWindow'
+
+const confirmText = document.createElement('h2')
+confirmText.className = 'confirmText'
+
+const emailField = document.createElement('input')
+emailField.className = 'textFieldMsg'
+
+const nameField = document.createElement('input')
+nameField.className = 'textFieldMsg'
+
+const bodyField = document.createElement('input')
+bodyField.className = 'textFieldMsg'
+
+const editBtn = document.createElement('button')
+editBtn.className = 'editBtn'
+editBtn.innerText = 'edit'
+
+const deleteBtn = document.createElement('button')
+deleteBtn.className = 'deleteBtn'
+deleteBtn.innerText = 'delete'
